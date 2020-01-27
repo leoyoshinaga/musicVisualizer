04b43625da1c9a641a266c47f6aaf7eff76e8a94
Leo Yoshinaga <yoshinagaleo@icloud.com>
2020-01-27 14:56:59 -0500

    functional

--- main.js
+++ main.js
@@ -3,16 +3,20 @@
 const SimplexNoise = require('simplex-noise')
 var noise = new SimplexNoise()
 
-let scene , camera, renderer;
+let scene , camera, renderer, ball, skybox
+//parsing music into an array
+var context = new AudioContext()
+var audio = document.getElementById('myAudio')
+var audioSource = context.createMediaElementSource(audio)
+var analyser = context.createAnalyser()
+audioSource.connect(analyser)
+analyser.connect(context.destination)
+analyser.fftSize = 512
+var dataArray = new Uint8Array(analyser.frequencyBinCount)
+
+//initiate THREE stuff
 function init(){
-  var context = new AudioContext()
-  var audio = document.getElementById('myAudio')
-  var audioSource = context.createMediaElementSource(audio)
-  var analyser = context.createAnalyser()
-  audioSource.connect(analyser)
-  analyser.connect(context.destination)
-  analyser.fftSize = 512
-  var dataArray = new Uint8Array(analyser.frequencyBinCount)
+
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 45, 30000 );
   camera.position.set(-900, -200, -900)
@@ -48,7 +52,7 @@ function init(){
   }
 
   let skyboxCube = new THREE.BoxGeometry(10000, 10000, 10000)
-  let skybox = new THREE.Mesh(skyboxCube, materialArray)
+  skybox = new THREE.Mesh(skyboxCube, materialArray)
   scene.add(skybox)
 
   var geometry = new THREE.BoxGeometry( 22, 22, 22 );
@@ -71,7 +75,7 @@ function init(){
       wireframe: true
   });
 
-  var ball = new THREE.Mesh(icosahedronGeometry, lambertMaterial)
+  ball = new THREE.Mesh(icosahedronGeometry, lambertMaterial)
   ball.position.set(0, 0, 0)
   scene.add(ball)
 
@@ -82,11 +86,58 @@ function init(){
 
 
 function animate() {
-  renderer.render(scene, camera)
-  requestAnimationFrame( animate );
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
+  analyser.getByteFrequencyData(dataArray);
+
+  var lowerHalfArray = dataArray.slice(0, (dataArray.length/2) - 1);
+  var upperHalfArray = dataArray.slice((dataArray.length/2) - 1, dataArray.length - 1);
+
+  var lowerMax = max(lowerHalfArray);
+  var upperAvg = avg(upperHalfArray);
+
+  var lowerMaxFr = lowerMax / lowerHalfArray.length;
+  var upperAvgFr = upperAvg / upperHalfArray.length;
+
+  makeRoughBall(ball, modulate(Math.pow(lowerMaxFr, 0.8), 0, 1, 0, 8), modulate(upperAvgFr, 0, 1, 0, 4));
+
+  ball.rotation.y += 0.005;
+  skybox.rotation.y += 0.005
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate)
 	renderer.render( scene, camera );
+}
+
+function makeRoughBall(mesh, bassFr, treFr) {
+  mesh.geometry.vertices.forEach(function (vertex, i) {
+      var offset = mesh.geometry.parameters.radius;
+      var amp = 7;
+      var time = window.performance.now();
+      vertex.normalize();
+      var rf = 0.00001;
+      var distance = (offset + bassFr ) + noise.noise3D(vertex.x + time *rf*7, vertex.y +  time*rf*8, vertex.z + time*rf*9) * amp * treFr;
+      vertex.multiplyScalar(distance);
+  });
+  mesh.geometry.verticesNeedUpdate = true;
+  mesh.geometry.normalsNeedUpdate = true;
+  mesh.geometry.computeVertexNormals();
+  mesh.geometry.computeFaceNormals();
 }
 init()
 
+function fractionate(val, minVal, maxVal) {
+  return (val - minVal)/(maxVal - minVal);
+}
+
+function modulate(val, minVal, maxVal, outMin, outMax) {
+  var fr = fractionate(val, minVal, maxVal);
+  var delta = outMax - outMin;
+  return outMin + (fr * delta);
+}
+
+function avg(arr){
+  var total = arr.reduce(function(sum, b) { return sum + b; });
+  return (total / arr.length);
+}
+
+function max(arr){
+  return arr.reduce(function(a, b){ return Math.max(a, b); })
+}
